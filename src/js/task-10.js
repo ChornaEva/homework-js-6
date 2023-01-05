13ead45d67eca1fb68ea1401012db632c6e4767c
Chorna Yevheniia <chornaja.evgenija@gmail.com>
2023-01-05 19:42:29 +0200

    task in progress

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,13 +1,5 @@
 // Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-
 // Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
 
 // Розміри найпершого <div> - 30px на 30px.
@@ -16,18 +8,31 @@
 
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 
+// <div id="controls">
+//   <input type="number" min="1" max="100" step="1" />
+//   <button type="button" data-create>Create</button>
+//   <button type="button" data-destroy>Destroy</button>
+// </div>
+
+// <div id="boxes"></div>
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const allElements = document.querySelector("#controls");
 const input = document.querySelector("input");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const divBoxesNode = document.querySelector("#boxes");
 
-input.addEventListener("input", () => {});
-createBtn.addEventListener("click", () => {});
-destroyBtn.addEventListener("click", () => {});
+let amount = 0;
+
+const onInputChange = (event) => {
+  amount = event.currentTarget.value;
+  console.log(amount);
+  // return amount;
+};
 
 const createBoxes = (amount) => {
   const divCollection = [];
@@ -36,6 +41,7 @@ const createBoxes = (amount) => {
   for (let i = 0; i < amount; i += 1) {
     const divElement = document.createElement("div");
     divElement.style.width = `${initSize + 10 * i}px`;
+    0;
     divElement.style.height = `${initSize + 10 * i}px`;
     divElement.style.backgroundColor = getRandomHexColor();
     divCollection.push(divElement);
@@ -43,4 +49,12 @@ const createBoxes = (amount) => {
 
   divBoxesNode.append(...divCollection);
 };
-createBoxes(5);
+// createBoxes(5);
+
+const destroyBoxes = () => {
+  divBoxesNode.innerHTML = "";
+};
+
+input.addEventListener("input", onInputChange);
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
